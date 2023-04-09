43dcd42b5a7f8477d6382700185327ee5c744c8b
Francisco Barros <114003387+chicaoterrordasidosas@users.noreply.github.com>
2023-04-09 10:34:52 -0300

    Update imc.java

--- imc.java
+++ imc.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ClasseScanner {
     public static void main(String[] args) {
+
         Scanner leia = new Scanner(System.in);
         String nome, resultado;
         float peso, altura, imc;
@@ -12,37 +13,29 @@ public class ClasseScanner {
         System.out.println("Insira Seu peso");
         peso = leia.nextFloat();
         imc = (peso / (altura * altura));
-        if (imc < 18.5)
-            ;
-        {
+        resultado = "";
+        if (imc < 18.5) {
+
             resultado = ("Muito magro");
         }
-        if (imc >= 18.5 && imc <= 24.9)
-            ;
-        {
+        if (imc >= 18.5 && imc <= 24.9) {
             resultado = ("Normal");
         }
-        if (imc >= 25.5 && imc <= 29.9)
-            ;
-        {
+        if (imc >= 25.5 && imc <= 29.9) {
             resultado = ("Sobrepeso");
         }
-        if (imc >= 30.0 && imc <= 34.9)
-            ;
-        {
+        if (imc >= 30.0 && imc <= 34.9) {
             resultado = ("Obeso Grau 1");
         }
-        if (imc >= 35.5 && imc <= 39.9)
-            ;
-        {
+        if (imc >= 35.5 && imc <= 39.9) {
             resultado = ("Obeso grau 2");
         }
-        if (imc >= 40.0)
-            ;
-        {
+        if (imc >= 40.0) {
+
             resultado = ("Obeso grau 3");
         }
-        System.out.println("Nome: " + nome);
+
+        System.out.println(imc);
         System.out.println("Peso: " + peso);
         System.out.println("Altura: " + altura);
         System.out.println("Resultado: " + resultado);
